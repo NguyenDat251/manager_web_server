0ad1200c499f8c4e2d845cb99d56a4d4f7f13e1e
Dat Nguyen <nguyenquocdat2511998@gmail.com>
2020-03-21 22:56:55 +0700

    add script database

---
+++ Script_project_database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) ,
+  `description` varchar(255) ,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
+
+INSERT INTO `project` VALUES ('1', 'project 1', 'abcaaaaaaaaaaaaaa');
+INSERT INTO `project` VALUES ('2', 'project 2', 'abcaaaaaaaaaaaaaa');
+INSERT INTO `project` VALUES ('3', 'project 3', 'abcaaaaaaaaaaaaaa');
